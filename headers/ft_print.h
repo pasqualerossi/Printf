456b4240a8e34a546c478bf3a76631a195c9d99f
Pasquale Rossi <58959408+pasz4@users.noreply.github.com>
2021-11-10 12:15:34 +1030

    Add files via upload

--- headers/ft_print.h
+++ headers/ft_print.h
@@ -6,12 +6,12 @@
 /*   By: prossi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/25 12:14:00 by prossi            #+#    #+#             */
-/*   Updated: 2021/11/09 12:52:25 by prossi           ###   ########.fr       */
+/*   Updated: 2021/11/10 12:12:54 by prossi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
+#ifndef FT_PRINT_H
+# define FT_PRINT_H
 
 # include <stdarg.h>
 # include <stdlib.h>
@@ -20,16 +20,16 @@
 
 typedef struct s_print
 {
-	va_list args;
+	va_list	args;
 	int		counter;
 }	t_print;
 
 void	ifchar(t_print *arg_count);
 void	ifstring(t_print *arg_count);
 void	ifpointer(t_print *arg_count);
+void	ifhex(t_print *arg_count);
 void	ifint(t_print *arg_count);
 void	ifunsignedint(t_print *arg_count);
-void	ifhex(t_print *arg_count);
 int		ft_printf(const char *str, ...);
 
 #endif
