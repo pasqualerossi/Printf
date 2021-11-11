5c3f0a42ad3046a224e291ccbfc9460d693ddee6
Pasquale Rossi <58959408+pasz4@users.noreply.github.com>
2021-11-11 13:38:24 +1030

    Add files via upload

--- srcs/hex-pointer-string-character.c
+++ srcs/hex-pointer-string-character.c
@@ -6,7 +6,7 @@
 /*   By: prossi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 12:08:22 by prossi            #+#    #+#             */
-/*   Updated: 2021/11/11 13:30:59 by prossi           ###   ########.fr       */
+/*   Updated: 2021/11/11 13:36:16 by prossi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,28 +56,28 @@ void	ifhex(t_print *arg_count, char character)
 
 void	ifpointer(t_print *arg_count)
 {
-	int				len;
-	char			*str;
-	unsigned long	num;
+	int				length;
+	char			*string;
+	unsigned long	number;
 
-	num = va_arg(arg_count->args, unsigned long);
-	len = 1;
-	str = numberlength(num, &len);
+	number = va_arg(arg_count->args, unsigned long);
+	length = 1;
+	string = numberlength(number, &length);
 	ft_putstr_fd("0x", 1);
 	arg_count->counter += 2;
-	len--;
-	while (len >= 0)
+	length--;
+	while (length >= 0)
 	{
-		if (num % 16 <= 10)
-			str[len] = '0' + (num % 16);
+		if (number % 16 <= 10)
+			string[length] = '0' + (number % 16);
 		else
-			str[len] = 'W' + (num % 16);
-		num = num / 16;
-		len--;
+			string[length] = 'W' + (number % 16);
+		number = number / 16;
+		length--;
 		arg_count->counter++;
 	}
-	ft_putstr_fd(str, 1);
-	free(str);
+	ft_putstr_fd(string, 1);
+	free(string);
 }
 
 void	ifcharacter(t_print *arg_count)
